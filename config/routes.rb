d8680a76079d8aed05d8d9f6e7a40c5e155a7c21
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-01-07 16:57:48 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
+    resources :messages, only: [:index, :create, :message_params, :set_group]
 end
